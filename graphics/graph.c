31043cc0c54df531660117fd6c72603fe632427d
zoomlogo <maanasyt@gmail.com>
2026-02-08 14:38:47 +0530

    signal processing be like

--- graphics/graph.c
+++ graphics/graph.c
@@ -35,3 +35,25 @@ void graph_draw_fft(sh110x_t *oled, const uint16_t *fft_mag, int bins) {
         sh110x_fill_rect(oled, x, y, bar_w - 1, h);
     }
 }
+
+void graph_draw_wave(sh110x_t *oled, float *samples, int len) {
+    int _x = GRAPH_X0;
+    int _y = GRAPH_Y1 / 2;
+
+    int step = len / GRAPH_WIDTH;
+    if (step < 1) step = 1;
+
+    for (int i = 0; i < GRAPH_WIDTH; ++i) {
+        int sample_index = i * step;
+        if (sample_index >= len) break;
+
+        float val = samples[sample_index];
+        int y = 32 - (int) (val * 32);
+        if (y < 0) y = 0; if (y > 63) y = 63;
+        int x = GRAPH_X0 + i;
+
+        sh110x_draw_line(oled, _x, _y, x, y);
+        _x = x;
+        _y = y;
+    }
+}
--- pico-audio-visualiser.c
+++ pico-audio-visualiser.c
@@ -16,29 +16,26 @@
 #define BCLK 4
 #define DATA 6
 
-#define SAMPLE_COUNT 256
+// audio stuff
+#define SAMPLE_COUNT 4096
+#define GAIN 16
+#define ALPHA 0.2
 
-uint32_t raw_buffer[SAMPLE_COUNT];
+uint32_t raw_buffer[2 * SAMPLE_COUNT];
+float hanning_table[SAMPLE_COUNT];
 
 int main() {
     stdio_init_all();
-        while (!stdio_usb_connected()) {
-        sleep_ms(100);
-    }
+    sleep_ms(100);
 
-
-    sleep_ms(2000);
-
-    printf("HELLO FROM PICO\n");
-
-    sleep_ms(1000);
-
+    // init i2c
     i2c_init(i2c0, 100000);
     gpio_set_function(8, GPIO_FUNC_I2C);
     gpio_set_function(9, GPIO_FUNC_I2C);
     gpio_pull_up(8);
     gpio_pull_up(9);
-    
+
+    // init mic
     PIO pio = pio0;
     int sm = 0;
 
@@ -54,125 +51,67 @@ int main() {
     channel_config_set_write_increment(&dc, true);
     channel_config_set_dreq(&dc, pio_get_dreq(pio, sm, false));
 
-    dma_channel_configure(
-        dma_chan,
-        &dc,
-        raw_buffer,
-        &pio->rxf[sm],
-        SAMPLE_COUNT,
-        true
-    );
+    // init oled
+    sh110x_t oled;
+    sh110x_init(&oled, i2c0, 0x3C);
 
-    // sh110x_clear(&oled);
 
-    // for (int i = 0; i < 64; i++) {
-    //     sh110x_draw_pixel(&oled, 128-i, 64-i, true);
-    // }
+    // precompute hanning
+    for (int i = 0; i < SAMPLE_COUNT; i++) {
+        hanning_table[i] = 0.5 * (1 - cos(2 * M_PI * i / (SAMPLE_COUNT - 1)));
+    }
 
-    // sh110x_draw_line(&oled, 0, 0, 127, 0);
-    // sh110x_draw_line(&oled, 0, 0, 0, 63);
-    // sh110x_draw_line(&oled, 0, 0, 127, 63);
-    // sh110x_draw_line(&oled, 64, 63, 64, 0);
-    // sh110x_draw_line(&oled, 10, 50, 100, 20);
-
-    // sh110x_draw_text(&oled, 1, 1, "FFT ONLINE", 1);
-    // sh110x_draw_text(&oled, 5, 15, "DRIVERS > SLEEP", 1);
-    
-    // uint16_t wave[32] = {
-    //     12, 15, 14, 13, 16, 15, 14, 13,
-    //     12, 14, 15, 16, 14, 13, 15, 14,
-    //     13, 12, 14, 15, 13, 14, 16, 15,
-    //     14, 13, 15, 14, 13, 12, 14, 13
-    // };
-
-    // double wave[64];
-    // double xr[64], xi[64];
-    // uint16_t mag[32];
-
-    // int dt = 0;
-    // for (;;) {
-    //     sh110x_clear(&oled);
-    //     // update
-    //     for (int i = 0; i < 64; i++) {
-    //         float y = cos((i + dt) / 32.0 * 2 * M_PI);
-    //         wave[i] = fabs(sin(dt)) * y;
-    //     }
-
-    //     // fft
-    //     fft2(wave, 64, xr, xi);
-
-    //     for (int i = 0; i < 32; ++i) {
-    //         mag[i] = sqrtf(xr[i + 32] * xr[i + 32] + xi[i + 32] * xi[i + 32]);
-    //     }
-
-    //     graph_draw_axes(&oled);
-    //     graph_draw_fft(&oled, mag, 32);
-
-    //     sh110x_update(&oled);
-    //     sleep_ms(100);
-
-    //     ++dt;
-    // }
-
+    // main loop
+    float wave[SAMPLE_COUNT];
+    uint16_t mags[SAMPLE_COUNT];
     while (1) {
-        // dma_channel_wait_for_finish_blocking(dma_chan);
-
-        // for (int i = 0; i < SAMPLE_COUNT; i += 2) {
-        //     // LEFT channel only
-        //     int32_t sample = (int32_t)(raw_buffer[i] << 8);
-        //     sample >>= 8;  // sign extend 24-bit
-
-        //     printf("%ld\n", sample);
-        //     // char buf[8];
-        //     // sprintf(buf, "%8ld", sample);
-        //     // sh110x_draw_text(&oled, 2, 2, buf, 1);
-        // }
-
-        // int32_t min =  1e9;
-        // int32_t max = -1e9;
-
-        // for (int i = 0; i < SAMPLE_COUNT; i += 2) {
-        //     // int32_t s = (int32_t)(raw_buffer[i] << 8) >> 8;
-        //     int32_t s = (int32_t)raw_buffer[i];
-        //     s >>= 8;
-        //     if (s < min) min = s;
-        //     if (s > max) max = s;
-        // }
-
-        // printf("min=%ld max=%ld\n", min, max);
-
-        dma_channel_wait_for_finish_blocking(dma_chan);
-
-        int32_t min =  100000000;
-        int32_t max = -100000000;
-
-        for (int i = 0; i < SAMPLE_COUNT; i += 2) {
-            int32_t s = (int32_t)raw_buffer[i];
-            if (s & 0x00800000) s |= 0xFF000000; // sign extend 24-bit
-
-            if (s < min) min = s;
-            if (s > max) max = s;
-        }
-
-        printf("min=%ld max=%ld\n", min, max);
-
-        dma_channel_set_read_addr(dma_chan, &pio->rxf[sm], true);
-
-
+        // read from sensor
         dma_channel_configure(
             dma_chan,
             &dc,
             raw_buffer,
             &pio->rxf[sm],
-            SAMPLE_COUNT,
+            2 * SAMPLE_COUNT,
             true
         );
+        dma_channel_wait_for_finish_blocking(dma_chan);
 
-        // sh110x_update(&oled);
+        int32_t min = 100000000;
+        int32_t max = -100000000;
+
+        float sum = 0; // to remove DC
+        for (int i = 0; i < 2 * SAMPLE_COUNT; i += 2) {
+            int32_t s = (int32_t) raw_buffer[i];
+            if (s & 0x00800000) s |= 0xFF000000; // sign extend 24-bit
+
+            float x = (float) s / 8388608.0f;
+            wave[i / 2] = x;
+            sum += x;
+        }
+
+        float av = sum / SAMPLE_COUNT;
+        float _x = 0;
+        // remove dc
+        for (int i = 0; i < SAMPLE_COUNT; ++i) {
+            wave[i] -= av;
+            wave[i] *= GAIN;
+
+            // apply low pass filtering (digital signal processing :fire:)
+            _x = ALPHA * wave[i] + (1 - ALPHA) * _x;
+            wave[i] = _x;
+        }
+
+        // hanning window
+        for (int i = 0; i < SAMPLE_COUNT; ++i) {
+            wave[i] *= hanning_table[i];
+        }
+
+        // display oled
+        sh110x_clear(&oled);
+
+        graph_draw_axes(&oled);
+        graph_draw_wave(&oled, wave, SAMPLE_COUNT);
+
+        sh110x_update(&oled);
     }
-
-
-
-
-    while (1) tight_loop_contents();
 }
